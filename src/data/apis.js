9bc3e437f63ca10fd928df0626ef5b15cc697b3d
Sagar Saini <sagarsaini6943@gmail.com>
2026-08-31 22:29:39 +0530

    feat: add User API endpoints for fetching user details and user by ID

--- src/data/apis.js
+++ src/data/apis.js
@@ -13,6 +13,34 @@ export const myApis = [
             "Content-Type": "application/json",
         },
     },
+    {
+        id: "user",
+        name: "User API",
+        description:
+            "Returns mock data of users details",
+        method: "GET",
+        endpoint: "https://golang-apis-dox2.onrender.com/users",
+        body: {
+            
+        },
+        headers: {
+            "Content-Type": "application/json",
+        },
+    },
+    {
+        id: "user{}",
+        name: "User API{}",
+        description:
+            "Returns mock data of user detail using it's ID number",
+        method: "GET",
+        endpoint: "https://golang-apis-dox2.onrender.com/users/{id}",
+        body: {
+            
+        },
+        headers: {
+            "Content-Type": "application/json",
+        },
+    },
 
     
 ];
