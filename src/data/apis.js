9d2f67ad1a751e6d2c19185f4b63ea8e9e1d37d6
Sagar Saini <sagarsaini6943@gmail.com>
2026-08-31 10:18:04 +0530

    fix: update Hello API to use GET method and return current time

--- src/data/apis.js
+++ src/data/apis.js
@@ -3,11 +3,11 @@ export const myApis = [
         id: "hello",
         name: "Hello API",
         description:
-            "Returns a greeting using the name provided in the request body.",
-        method: "POST",
-        endpoint: "https://golang-apis-dox2.onrender.com/api/v1/greet",
+            "Returns a greeting with current time.",
+        method: "GET",
+        endpoint: "https://golang-apis-dox2.onrender.com/greet",
         body: {
-            name: "Sagar",
+            
         },
         headers: {
             "Content-Type": "application/json",
